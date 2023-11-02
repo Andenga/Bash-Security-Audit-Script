099802879fbee2ae46803c15254777ea7e58b2d2
Andenga <62437418+Andenga@users.noreply.github.com>
2023-11-02 15:14:18 +0300

    Edit

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,19 +9,15 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    // Allocate memory for a new binary tree node
     binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-    // Check if memory allocation was successful
     if (node == NULL)
         return (NULL);
 
-    // Initialize the new node with the provided value and parent node
     node->n = value;
     node->parent = parent;
     node->left = NULL;
     node->right = NULL;
 
-    // Return a pointer to the newly created node
     return (node);
 }
